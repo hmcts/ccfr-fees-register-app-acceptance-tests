0a1fc96319f8f5b4d4d8f3e4f56f36ef68b3a887
Krishna Kishore Nuthalapati <krishnakishorenuthalapati@Krishnas-MacBook-Pro.local>
2017-08-15 11:51:15 +0100

    FeesRegister API Database Acceptance Tests

---
+++ src/test/java/uk/gov/hmcts/feesregister/acceptancetests/IntegrationTestBase.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.feesregister.acceptancetests;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestContextConfiguration.class)
+public class IntegrationTestBase {
+}
